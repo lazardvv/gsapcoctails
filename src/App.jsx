40469a4322c1ca1742ff341d32d7b204400b00a5
Lazar Drazic <lazar.drazic@htecgroup.com>
2025-07-07 10:35:52 +0200

    initital commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { ScrollTrigger,SplitText } from 'gsap/all'
+import gsap from 'gsap';
+
+gsap.registerPlugin(ScrollTrigger,SplitText);
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex-center h-[100vh]'>
+      <h1 className='text-3xl text-indigo-300'>App</h1>
+    </div>
+  )
+}
+
+export default App
