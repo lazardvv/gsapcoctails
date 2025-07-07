509e69bc853e38d99f6a62ec0333c934c5cadb87
Lazar Drazic <lazar.drazic@htecgroup.com>
2025-07-07 14:52:27 +0200

    implement hero scroll animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import { useGSAP } from '@gsap/react'
-import { SplitText } from 'gsap/all';
+import { SplitText } from 'gsap/SplitText';
 import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useRef } from 'react'
+import { useMediaQuery } from 'react-responsive';
+
+// Register GSAP plugins
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const Hero = () => {
+    const videoRef = useRef();
+
+    const isMobile = useMediaQuery({ maxWidth: 767 })
+
+
+
     useGSAP(() => {
         const heroSplit = new SplitText('.title', { type: 'chars, words' });
         const paragraphSplit = new SplitText('.subtitle', { type: 'lines' });
@@ -27,17 +39,36 @@ const Hero = () => {
             delay: 1
         });
 
-gsap.timeline({
-  scrollTrigger: {
-    trigger: '#hero',
-    start: 'top top',
-    end: 'bottom top',
-    scrub: true
-  }
-})
-.to('.right-leaf', { y: 200 }, 0)
-.to('.left-leaf', { y: -200 }, 0);
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: '#hero',
+                start: 'top top',
+                end: 'bottom top',
+                scrub: true
+            }
+        })
+            .to('.right-leaf', { y: 200 }, 0)
+            .to('.left-leaf', { y: -200 }, 0);
 
+        const startValue = isMobile ? 'top 50%' : 'center 60%'
+        const endValue = isMobile ? '120% top' : 'bottom top'
+
+        // Create a timeline for video animation
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'video',
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,               
+            }
+        });
+
+        videoRef.current.onloadedmetadata = () => {
+            tl.to(videoRef.current, {
+                currentTime: videoRef.current.duration
+            })
+        }
 
     }, [])
 
@@ -65,6 +96,18 @@ gsap.timeline({
                     </div>
                 </div>
             </section>
+            <div className='video absolute inset-0'>
+                <video
+                    src='/videos/output.mp4'
+                    ref={videoRef}
+                    muted
+                    playsInline
+                    preload='auto'
+                    onLoadedMetadata={() => {
+                    
+                    }}
+                />
+            </div>
         </>
     )
 }
